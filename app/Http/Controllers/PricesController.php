be6aa02a90704fbb225b9b70303b243cc477694d
Nadir Eliel Jaramillo Rosas <nadirjaramillorosas@hotmail.com>
2023-04-02 18:42:51 -0300

    initial commit

---
+++ app/Http/Controllers/PricesController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Prices;
+
+class PricesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $prices = Prices::all();
+        return \response($prices);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'course' => 'required',
+            'price' => 'required',
+        ]);
+        $price = Prices::create($request->all());
+        return \response($price);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $price = Prices::findOrFail($id);
+        return \response($price);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $price = Prices::findOrFail($id)
+                        ->update($request->all());
+        return \response("Updated");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
